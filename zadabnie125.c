dddfdceee3fe49a7f2a9f5471587125ba78398c8
Eryk Ł <eryk.lechtanski81@gmail.com>
2021-01-28 21:42:08 +0100

    dodanie drugich obliczen

--- zadabnie125.c
+++ zadabnie125.c
@@ -10,9 +10,22 @@ int objetosc_prostopadl(int a, int b, int H)
     return V;
 }
 
+int objetosc_ostroslup(int a, int H)
+{
+    int V;
+    V = (a * a * H) / 3;
+
+    return V;
+}
+
 int main(void)
 {
     int a, b, H, V, figura;
+
+    printf("Objetosc jakiej figury chcesz policzyc?\nJezeli prostopadloscian, to wpisz '1'; a jesli ostoslup prawidlowy czworokatny, to wpisz '2': ");
+    scanf ("%d", &figura);
+
+    if (figura == 1) {
 
     printf("Podaj kolejno w wierszach ponizej dlugosc podstawy, szerokosc podstawy oraz wysokosc figury: \n");
     scanf("%d %d %d", &a, &b, &H);
@@ -26,6 +39,25 @@ int main(void)
     printf("Objetosc prostopadloscianu wynosi: %d \n", V);
     printf("\n");
         }
+    }
 
+    else if (figura == 2){
+
+        printf("Podaj kolejno w wierszach poniżej dlugosc krawedzi podstawy oraz wysokosc figury: \n");
+        scanf("%d %d", &a, &H);
+
+         if (a <= 0 || H <= 0){
+        printf("Podano zle wartosci!!");
+
+    } else {
+
+    V = objetosc_ostroslup(a, H);
+    printf("Objetosc ostroslupa wynosi: %d \n", V);
+    printf("\n");
+            }
+    }
+    else {
+        printf("Wpisano niewlasciwe dane!\n");
+    }
 return 0;
     }
